3fd92a0503734fa214bf2bda25812e3c31e9aa6c
harsha kurra <harshavardhankurra4@gmail.com>
2021-04-08 00:04:33 -0500

    add vegetables using to postman

--- controllers/vegetables.js
+++ controllers/vegetables.js
@@ -19,9 +19,25 @@ exports.vegetable_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: vegetable detail: ' + req.params.id);
 };
 // Handle vegetable create on POST.
-exports.vegetable_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: vegetable create POST');
-};
+// Handle Costume create on POST.
+exports.vegetable_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new vegetable();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.color = req.body.color;
+    document.weight = req.body.weight;
+    document.vitamins = req.body.vitamins;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
 // Handle vegetable delete form on DELETE.
 exports.vegetable_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: vegetable delete DELETE ' + req.params.id);
