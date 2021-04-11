9a98d9116da8c16d7a8416519f4f71f07ae02f7c
harsha kurra <harshavardhankurra4@gmail.com>
2021-04-11 14:48:07 -0500

    update page in vegetable

--- controllers/vegetables.js
+++ controllers/vegetables.js
@@ -112,3 +112,17 @@ exports.vegetable_create_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.vegetable_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await vegetable.findById(req.query.id)
+        res.render('vegetableupdate', { title: 'vegetable Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
